563bdef6ce0f61f23bc8c8b1e7363ec905319671
Evil00787 <matititi123@gmail.com>
2019-09-07 18:51:41 +0200

    Added networkController to HistoryController

--- Assets/Scripts/Controllers/HistoryController.cs
+++ Assets/Scripts/Controllers/HistoryController.cs
@@ -4,21 +4,25 @@ namespace Controllers {
 	public class HistoryController : MonoBehaviour {
 
 		private NodeController nodeController;
+		private NetworkController networkController;
 		public HistoryService historyService;
 
 		bool nodeChangedByHistory = false;
 
 		void Awake() {
-			nodeController = GetComponent<NodeController>();
-			historyService = new HistoryService();
-			nodeController.OnSelectedNodeChanged += (oldNode, newNode) => {
-				if (!nodeChangedByHistory) historyService.RegisterAction(new NodeSelectedAction(oldNode, newNode));
-				nodeChangedByHistory = false;
-			};
-			NodeSelectedAction.selectNodeAction = node => {
-				nodeChangedByHistory = true;
-				nodeController.ForceSetSelect(node);
-			};
+			networkController = GetComponent<NetworkController>();
+			if (networkController.IsServer()) {
+				nodeController = GetComponent<NodeController>();
+				historyService = new HistoryService();
+				nodeController.OnSelectedNodeChanged += (oldNode, newNode) => {
+					if (!nodeChangedByHistory) historyService.RegisterAction(new NodeSelectedAction(oldNode, newNode));
+					nodeChangedByHistory = false;
+				};
+				NodeSelectedAction.selectNodeAction = node => {
+					nodeChangedByHistory = true;
+					nodeController.ForceSetSelect(node);
+				};
+			}
 		}
 	}
 }
