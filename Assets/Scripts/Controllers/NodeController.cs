289a9653193996b3db0e96f58b146e729a47c931
Stanisław Góra <stasgora@gmail.com>
2019-09-22 16:34:52 +0200

    Node state change fixes

--- Assets/Scripts/Controllers/NodeController.cs
+++ Assets/Scripts/Controllers/NodeController.cs
@@ -33,6 +33,7 @@ namespace Controllers {
 		public Node HighlightedNode {
 			get { return highlightedNode; }
 			set {
+				Debug.Log(value?.State);
 				if (highlightedNode == value || value != null && value.State == NodeState.DISABLED) return;
 				if (highlightedNode != null) {
 					if (highlightedNode.State != NodeState.SELECTED)
@@ -116,6 +117,8 @@ namespace Controllers {
 
 		private void UpdateNodeStates() {
 			SetAllNodesAs(DefaultState);
+			if(HighlightedNode != null)
+				SetNodeState(HighlightedNode, NodeState.HIGHLIGHTED);
 			if (graphController.GraphMode.Value == GraphMode.NODE_TRAVERSE) {
 				GraphController.Graph.ConnectionObjectMap.Keys.Where(connection => connection.Ends.Contains(SelectedNode)).ToList()
 					.ForEach(connection => UpdateConnectionEndStates(connection, NodeState.ACTIVE));
@@ -154,8 +157,14 @@ namespace Controllers {
 			if(graphController.GraphMode.Value == GraphMode.FREE_FLIGHT)
 				return;
 			var ends = connection.Ends;
-			if(ends.Contains(SelectedNode))
-				SetNodeState(ends[ends.IndexOf(SelectedNode) == 0 ? 1 : 0], state);
+			if(ends.Contains(SelectedNode)) {
+				var otherNode = ends[ends.IndexOf(SelectedNode) == 0 ? 1 : 0];
+				if (otherNode == HighlightedNode && state == NodeState.DISABLED)
+					HighlightedNode = null;
+				SetNodeState(otherNode, state);
+				if(otherNode == HighlightedNode && state == NodeState.ACTIVE)
+					SetNodeState(otherNode, NodeState.HIGHLIGHTED);
+			}
 		}
 
 		#endregion
