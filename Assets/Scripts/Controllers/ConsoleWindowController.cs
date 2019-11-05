1949384c4dfc071817b727afb3454c93624d03c4
Mikolaj Mirko <mikolajmirko@gmail.com>
2019-11-05 14:09:21 +0100

    Console Window UI

---
+++ Assets/Scripts/Controllers/ConsoleWindowController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Model;
+
+namespace Controllers {
+	public class ConsoleWindowController : MonoBehaviour {
+
+		public GameObject ConsoleWindowCanvas;
+
+		private bool isServer;
+		private NetworkController networkController;
+		private InputController inputController;
+		private NodeController nodeController;
+		private ConnectionController connectionController;
+
+		// Use this for initialization
+		void Awake() {
+			networkController = GetComponent<NetworkController>();
+			inputController = GetComponent<InputController>();
+			nodeController = GetComponent<NodeController>();
+			connectionController = GetComponent<ConnectionController>();
+			isServer = networkController.IsServer();
+		}
+
+		// Use this for initialization
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
