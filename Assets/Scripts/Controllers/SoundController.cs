9793fa820653f16ec648eb12f0a0b77606648e46
Stanisław Góra <stasgora@gmail.com>
2019-11-24 12:19:47 +0100

    Sticky sound function

--- Assets/Scripts/Controllers/SoundController.cs
+++ Assets/Scripts/Controllers/SoundController.cs
@@ -1,4 +1,6 @@
-﻿using Controllers;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Controllers;
 using Model;
 using Services;
 using UnityEngine;
@@ -7,8 +9,9 @@ public class SoundController : MonoBehaviour {
 	public GameObject Graph;
 	[Range(1, 50)]
 	public float MaxNodeSpacialSoundDist = 10f;
-	
-	private SoundManager SoundManager;
+
+	private List<AudioSource> sources = new List<AudioSource>();
+	private SoundManager soundManager;
 	private Transform player;
 	
 	private NodeController nodeController;
@@ -20,23 +23,32 @@ public class SoundController : MonoBehaviour {
 		player = Graph.GetComponent<InputController>().Eyes.transform;
 		transform.parent = player;
 		
-		SoundManager = new SoundManager();
+		soundManager = new SoundManager();
 		var Audio = GetComponent<AudioSource>();
-		Audio.clip = SoundManager.Sounds[SoundType.AMBIENT_LOOP];
+		Audio.clip = soundManager.Sounds[SoundType.AMBIENT_LOOP];
 		Audio.Play();
 		
 		nodeController = Graph.GetComponent<NodeController>();
 		graphController = Graph.GetComponent<GraphController>();
-		nodeController.OnSelectedNodeChanged += (oldNode, newNode) => PlayLocalSound(SoundType.NODE_SELECTED);
+		nodeController.OnSelectedNodeChanged += (oldNode, newNode) => PlayStickySound(SoundType.NODE_SELECTED);
 	}
 
 	private void PlaySpacialNodeSound(Node node, SoundType sound) {
 		Vector3 direction = GetNodePosition(node) - player.position;
-		AudioSource.PlayClipAtPoint(SoundManager.Sounds[sound], player.position + direction * (MaxNodeSpacialSoundDist / graphController.WorldRadius));
+		AudioSource.PlayClipAtPoint(soundManager.Sounds[sound], player.position + direction * (MaxNodeSpacialSoundDist / graphController.WorldRadius));
 	}
 
 	private void PlayLocalSound(SoundType sound) {
-		AudioSource.PlayClipAtPoint(SoundManager.Sounds[sound], player.position);
+		AudioSource.PlayClipAtPoint(soundManager.Sounds[sound], player.position);
+	}
+
+	private void PlayStickySound(SoundType sound) {
+		var audioSource = sources.FirstOrDefault(source => !source.isPlaying);
+		if (audioSource == null) {
+			audioSource = gameObject.AddComponent<AudioSource>();
+			sources.Add(audioSource);
+		}
+		audioSource.PlayOneShot(soundManager.Sounds[sound]);
 	}
 
 	private Vector3 GetNodePosition(Node node) => GraphController.Graph.NodeObjectMap[node].transform.position;
